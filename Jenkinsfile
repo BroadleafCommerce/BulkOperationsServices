d57193407e44b5e23c6627111715ec9bcaed7836
Marie Standeven <marie.standeven@gmail.com>
2024-01-31 09:34:05 -0600

    Initial commit to setup Bulk Operations microservice

---
+++ Jenkinsfile
@@ -0,0 +1,8 @@
+mavenBuild {
+  extraBuildProfiles = '-Pdeptrack'
+  dependencyCheck = 'enabled'
+  dependencyCheckBomName = 'BulkOperationsService'
+  extraDeployProfiles = '-Pdocker'
+  javadocsDirectories = ['services/target']
+  buildAgentLabel = 'build-jdk17'
+}
---
+++ services/src/main/java/com/broadleafcommerce/bulkoperations/autoconfigure/BulkOperationsEnvironmentPostProcessor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2009 - 2020 Broadleaf Commerce
+ *
+ * Licensed under the Broadleaf End User License Agreement (EULA), Version 1.1 (the
+ * "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt).
+ *
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the
+ * "Custom License") between you and Broadleaf Commerce. You may not use this file except in
+ * compliance with the applicable license.
+ *
+ * NOTICE: All information contained herein is, and remains the property of Broadleaf Commerce, LLC
+ * The intellectual and technical concepts contained herein are proprietary to Broadleaf Commerce,
+ * LLC and may be covered by U.S. and Foreign Patents, patents in process, and are protected by
+ * trade secret or copyright law. Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained from Broadleaf Commerce, LLC.
+ */
+package com.broadleafcommerce.bulkoperations.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+/**
+ * Default property overrides for Bulk Operations Service
+ */
+public class BulkOperationsEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    private static final String YAML_CLASSPATH_LOCATION = "bulkoperations-defaults.yml";
+
+    private YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment,
+            SpringApplication application) {
+        try {
+            loader.load("Broadleaf Bulk Operations Service -- Defaults",
+                    new ClassPathResource(YAML_CLASSPATH_LOCATION))
+                    .forEach(ps -> environment.getPropertySources().addLast(ps));
+        } catch (IOException e) {
+            throw new IllegalStateException(
+                    "Could not load the Bulk Operations service property defaults",
+                    e);
+        }
+    }
+
+}
